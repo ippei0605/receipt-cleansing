454249c0e68255c5927e0c011fd475e6be325d1a
Ippei SUZUKI <ippei0605@gmail.com>
2022-02-12 22:37:49 +0900

    新規作成

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author='Ippei Suzuki',
     author_email='ippei0605@gmail.com',
     url='https://github.com/ippei0605/scskdai',
-    py_modules=['scskdai', '  receipt_cleansing']
+    py_modules=['scskdai.scskdai', 'scskdai.receipt_cleansing']
 )
