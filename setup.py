b6f2c27fb07ba42d56e670d0e49f5900a586007e
Ippei SUZUKI <ippei0605@gmail.com>
2022-02-12 13:00:43 +0900

    新規作成

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="scskdai",
+    version='1.0',
+    description='SCSKDAI データ分析のための共通機能',
+    author='Ippei Suzuki',
+    author_email='ippei0605@gmail.com',
+    url='https://github.com/ippei0605/scskdai',
+)
