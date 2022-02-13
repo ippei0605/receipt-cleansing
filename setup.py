6ff07725ec9f63ca20d1e1f6bb8f4b8c0d163222
Ippei SUZUKI <ippei0605@gmail.com>
2022-02-13 12:04:28 +0900

    新規作成

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="scskdai",
-    version='0.1.3',
+    version='0.1.4',
     description='SCSKDAI Common Function',
     author='Ippei Suzuki',
     author_email='ippei0605@gmail.com',
     url='https://github.com/ippei0605/scskdai',
+    packages=find_packages(),
 )
